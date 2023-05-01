13767eb5420fc172af9b924b7c5aab8ec7acfee7
Dean Moore <deanmoore009@gmail.com>
2023-05-01 17:42:11 +0100

    basic gui

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import PySimpleGUI as sg
+import sys
 
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+import PySimpleGUI as psg
+menu_def = [['File', ['New', 'Open', 'Save', 'Exit', ]], ['Edit', ['Cut', 'Copy', 'Paste', 'Undo'], ],  ['Help', 'About...'], ]
+layout = [[psg.Menu(menu_def)],
+   [psg.Multiline("", key='-IN-',
+   expand_x=True, expand_y=True)],
+   [psg.Multiline("", key='-OUT-',
+   expand_x=True, expand_y=True)],
+   [psg.Text("", key='-TXT-',
+   expand_x=True, font=("Arial Bold", 14))]
+]
+window = psg.Window("Menu", layout, size=(715, 300))
 while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-    print('You entered ', values[0])
+   event, values = window.read()
+   print(event, values)
 
+   if event != psg.WIN_CLOSED:
+      window['-TXT-'].update(values[0] + "Menu Button Clicked")
+   if event == 'Copy':
+      txt = window['-IN-'].get()
+   if event == 'Paste':
+      window['-OUT-'].update(value=txt)
+   if event == psg.WIN_CLOSED:
+      break
+      
 window.close()
+
+
